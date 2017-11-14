f7d20bef5e68e14d3cf34bf0b9db1584a8d65fdb
Andres Root <info@andresroot.co>
2017-11-13 19:43:46 -0800

    Work on bot

--- bot.py
+++ bot.py
@@ -1,6 +1,18 @@
 import os
 from dotenv import load_dotenv
+import tweepy
 
-
+# Manage credentials
 dotenv_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '.env')
 load_dotenv(dotenv_path)
+
+# Load credentials
+consumer_key = os.environ.get('CONSUMER_KEY')
+consumer_secret = os.environ.get('CONSUMER_SECRET')
+access_token = os.environ.get('ACCESS_TOKEN')
+access_token_secret = os.environ.get('ACCESS_TOKEN_SECRET')
+
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
